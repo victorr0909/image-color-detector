cb06a285de43679942f01e1b4c7e370e5b40e738
victorr0909 <namit09092001@gmail.com>
2024-03-31 00:00:25 +0530

    Final commit

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
 import colorgram
 
-# Extract 6 colors from an image.
-colors = colorgram.extract('image.jpeg', 6)
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    rgb_colors.append(color.rgb)
 
-# colorgram.extract returns Color objects, which let you access
-# RGB, HSL, and what proportion of the image was that color.
-first_color = colors[0]
-rgb = first_color.rgb # e.g. (255, 151, 210)
-hsl = first_color.hsl # e.g. (230, 255, 203)
-proportion  = first_color.proportion # e.g. 0.34
-
-# RGB and HSL are named tuples, so values can be accessed as properties.
-# These all work just as well:
-red = rgb[0]
-red = rgb.r
-saturation = hsl[1]
-saturation = hsl.s
+print(rgb_colors)
